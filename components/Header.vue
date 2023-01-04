4946b103a520e551d7aca8faa4183bdc650de6b9
MD IBRAHIM KHALIL <89800087+Mdibrahimkhalil78@users.noreply.github.com>
2023-01-04 21:01:05 +0600

    last commit

---
+++ components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="fixed left-0 top-0 w-full bg-white z-10 border-b">
+    <div class="mx-auto container px-6">
+      <div class="relative flex items-center justify-between">
+
+        <div class="relative py-3 flex justify-start items-center">
+          <span class="px-2 py-1 text-2xl text-gray-500 font-bold rounded-full border-2 border-gray-500 ">IK</span>
+        </div>
+
+        <nav class="flex space-x-0 sm:space-x-4">
+          <NuxtLink to="/" class="text-gray-500 px-2 py-2 text-lg font-bold">
+
+            <div class="relative before:z-10 before:absolute before:left-1/2 before:-bottom-3 before:w-max before:max-w-xs before:-translate-x-1/2 before:translate-y-full before:rounded-lg before:bg-gray-500 before:px-2 before:py-1.5 before:text-white before:invisible before:content-[attr(data-tip)] after:z-10 after:absolute after:left-1/2 after:-bottom-3 after:h-0 after:w-0 after:-translate-x-1/2 after:border-8 after:border-b-gray-500 after:border-l-transparent after:border-t-transparent after:border-r-transparent after:invisible hover:before:visible hover:after:visible" data-tip="Home">
+
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+              </svg>
+            </div>
+          </NuxtLink>
+          <NuxtLink to="/about" class="text-gray-500 px-2 py-2 text-lg font-bold">
+            <div class="relative before:z-10 before:absolute before:left-1/2 before:-bottom-3 before:w-max before:max-w-xs before:-translate-x-1/2 before:translate-y-full before:rounded-lg before:bg-gray-500 before:px-2 before:py-1.5 before:text-white before:invisible before:content-[attr(data-tip)] after:z-10 after:absolute after:left-1/2 after:-bottom-3 after:h-0 after:w-0 after:-translate-x-1/2 after:border-8 after:border-b-gray-500 after:border-l-transparent after:border-t-transparent after:border-r-transparent after:invisible hover:before:visible hover:after:visible" data-tip="About">
+
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+          </svg>
+            </div>
+          </NuxtLink>
+        </nav>
+
+      </div>
+    </div>
+  </header>
+  <div class="gap h-16"></div>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <Head>
+    <Title>About  - All Quran app</Title>
+  </Head>
+
+  <div class="flex items-center justify-center flex-col text-gray-500">
+    <h1 class="text-2xl font-bold text-center">All Quran App</h1>
+    <p>Version 1.0.0 (Beta)</p>
+    <p>Api: <a class="underline" href="https://alquran.cloud/api" target="_blank">Al Quran Cloud</a></p>
+  </div>
+
+  <div class="gap h-12"></div>
+
+  <Footer></Footer>
+</template>
